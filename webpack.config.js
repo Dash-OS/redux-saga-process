140eb53470b6a81f88c3fdd47b535d76a3df3188
Braden Napier <brady.napier@dashos.net>
2017-06-19 17:28:56 -0700

    update 1.0 build and publish as 1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,52 @@ var path = require('path');
 var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 //var env = require('yargs').argv.mode;
-var libraryName = 'redux-saga-process'
-var plugins = [], outputFile;
+var libraryName = 'redux-saga-process';
+var plugins = [],
+  outputFile;
 const BabiliPlugin = require('babili-webpack-plugin');
 const NodeExternals = require('webpack-node-externals');
 
-console.log('Env: ', process.env.NODE_ENV)
+console.log('Env: ', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    sourceMap: false,
-    compress: {
-      screw_ie8: true,
-      warnings: false,
-    },
-    mangle: {
-      screw_ie8: true,
-    },
-    output: {
-      comments: false,
-      screw_ie8: true,
-    },
-  }));
+  // plugins.push(
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     sourceMap: false,
+  //     compress: {
+  //       screw_ie8: true,
+  //       warnings: false,
+  //     },
+  //     mangle: {
+  //       screw_ie8: true,
+  //     },
+  //     output: {
+  //       comments: false,
+  //       screw_ie8: true,
+  //     },
+  //   }),
+  // );
   outputFile = libraryName + '.min.js';
-  plugins.push(new webpack.LoaderOptionsPlugin({
-    minimize: true,
-  }))
-  plugins.push(new webpack.DefinePlugin({
-    'process.env.NODE_ENV': process.env.NODE_ENV
-  }))
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  );
+  plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': process.env.NODE_ENV,
+    }),
+  );
 } else {
   outputFile = libraryName + '.js';
 }
 
 module.exports = {
-
   entry: [
     path.resolve(__dirname, './src/statics.js'),
-    path.resolve(__dirname, './src/main.js')
+    path.resolve(__dirname, './src/main.js'),
   ],
 
-  target: 'async-node',
+  // target: 'async-node',
 
   devtool: process.env.NODE_ENV !== 'production' && 'source-map',
 
@@ -50,13 +56,11 @@ module.exports = {
     filename: 'redux-saga-process.js',
     library: libraryName,
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
 
   resolve: {
-    modules: [
-      'node_modules'
-    ],
+    modules: ['node_modules'],
   },
 
   plugins: plugins,
@@ -66,36 +70,37 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        include: [
-            path.resolve(__dirname, './src')
-          ],
-        use: [ {
-          loader: 'babel-loader',
-
-          options: {
-            plugins: ['transform-class-properties'],
-            presets: [
-              ['env', {
-                modules: false,
-                targets: {
-                  browsers: ['last 2 Chrome versions'],
-                  node: 'current'
-                }
-              }],
-            ],
-            env: {
-              production: {
-                presets: [ 'babili' ]
-              }
-            }
-          }
-        } ]
-
-
-      }
-    ]
+        include: [path.resolve(__dirname, './src')],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              plugins: ['transform-class-properties'],
+              presets: [
+                [
+                  'env',
+                  {
+                    modules: false,
+                    targets: {
+                      browsers: ['last 2 Chrome versions'],
+                      node: 'current',
+                    },
+                  },
+                ],
+                'stage-0',
+              ],
+              env: {
+                production: {
+                  presets: ['babili'],
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
 
-  externals: [ NodeExternals() ]
-
-}
+  externals: [NodeExternals()],
+};
